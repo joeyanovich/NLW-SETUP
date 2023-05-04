77921e8fad5a4092306f288254c33c0ae8ec817a
joeyanovich <joedison.dias@gmail.com>
2023-05-04 11:19:59 -0300

    web: update conection with database

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,6 +1,7 @@
+import './style/global.css'
+import './lib/dayjs'
 import { Header } from './components/Header'
 import { SummaryTable } from './components/SummaryTable'
-import './style/global.css'
 
 // import { Habit } from "./components/Habit";
 
