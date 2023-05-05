06b49b310df7034d6e3177cf205833d9edce580d
joeyanovich <joedison.dias@gmail.com>
2023-05-05 10:31:39 -0300

    web: full habits sync

--- web/src/components/HabitsList.tsx
+++ web/src/components/HabitsList.tsx
@@ -1,4 +1,3 @@
-
 import * as Checkbox from '@radix-ui/react-checkbox';
 import { Check } from 'phosphor-react';
 import { useEffect, useState } from 'react';
@@ -6,7 +5,8 @@ import { api } from '../lib/axios';
 import dayjs from 'dayjs';
 
 interface HabitsListProps {
-    date: Date
+    date: Date;
+    onCompletedChanged: (completed: number) => void
 }
 
 interface HabitsInfo {
@@ -19,7 +19,7 @@ interface HabitsInfo {
 }
 
 
-export function HabitsList ({ date }: HabitsListProps) {
+export function HabitsList ({ date, onCompletedChanged }: HabitsListProps) {
     const [habitsInfo, setHabitsInfo] = useState<HabitsInfo>()
 
     useEffect(() => {
@@ -32,6 +32,27 @@ export function HabitsList ({ date }: HabitsListProps) {
         })
     }, [])
 
+    async function handleToggleHabit(habitId: string) {
+        await api.patch(`/habits/${habitId}/toggle`)
+
+        const isHabitAlreadyCompleted = habitsInfo!.completedHabits.includes(habitId)
+
+        let completedHabits: string[] = []
+
+        if(isHabitAlreadyCompleted) {
+            completedHabits = habitsInfo!.completedHabits.filter(id => id !== habitId)
+            
+        } else {
+            completedHabits = [...habitsInfo!.completedHabits, habitId]
+        }
+        setHabitsInfo({
+            possibleHabits: habitsInfo!.possibleHabits,
+            completedHabits,
+        })
+
+        onCompletedChanged(completedHabits.length)
+    }
+
     const isDateInPast = dayjs(date).endOf('day').isBefore( new Date())
 
     return (
@@ -40,6 +61,7 @@ export function HabitsList ({ date }: HabitsListProps) {
                 return (
                     <Checkbox.Root
                         key={habit.id}
+                        onCheckedChange={() => handleToggleHabit(habit.id)}
                         checked={habitsInfo.completedHabits.includes(habit.id)}
                         disabled={isDateInPast}
                         className='flex items-center gap-3 group'
